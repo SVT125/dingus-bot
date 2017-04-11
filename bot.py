6c066c64db05e9b2a0781b6c3ab11f4d1bd31c10
svt125 <jhardjadinata14@gmail.com>
2017-04-10 21:39:12 -0700

    Add send file command

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-import discord
 from discord.ext import commands
+import os
+import random
+import discord
 import asyncio
 
 description="A bot that provides useless commands and tidbits. I can be found at https://github.com/SVT125/dingus-bot."
@@ -11,9 +13,43 @@ async def on_ready():
     print('Logged in as {} (ID: {})'.format(bot.user.name, bot.user.id))
     print('------')
 
-@bot.command()
-async def test():
-    await bot.say("6")
+
+@bot.command(pass_context=True)
+async def file(ctx, path=""):
+    """
+    If a path is given as an argument, sends the data file found.
+    Otherwise, if the argument is just a string, returns a random file with the string contained in its name.
+    Else, sends a random file.
+    """
+    matched_files = []
+    is_file_path = lambda p: "." in path or "\\" in path
+    match_str = path
+
+    # If we're looking to match the argument string instead, we want to traverse all subdirs of data\.
+    if path and is_file_path(path):
+        sep = path.rfind('\\')
+        match_str = path[sep+1:]
+        path = path[0:sep]
+    elif path and not is_file_path(path):
+        path = ""
+
+    for root, dirs, files in os.walk('data\\' + path):
+        for file_name in files:
+            if (is_file_path(path) and file_name == path) or \
+                    (match_str and match_str in file_name) or \
+                    not match_str:
+                matched_files.append(os.path.join(root, file_name))
+
+    if matched_files:
+        chosen_file = random.choice(matched_files)
+        await bot.send_file(ctx.message.channel, chosen_file)
+    else:
+        if is_file_path(path):
+            await bot.say("Could not find a file with the given path name.")
+        elif path:
+            await bot.say("Could not find a file whose name contained the given string.")
+        else:
+            await bot.say("There are no files in this bot's data folder to send.")
 
 
 def get_bot():
